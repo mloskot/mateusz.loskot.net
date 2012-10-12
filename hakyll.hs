220f1ca9e883820de1398aeba61ecb736eb5efbb
Mateusz Loskot <mateusz@loskot.net>
2012-10-13 00:09:31 +0100

    Added templates for rendering of basic blog. Added sample blog posts. Configured some site metadata.

--- hakyll.hs
+++ hakyll.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Arrow ((>>>))
+module Main where
+
+import Prelude hiding (id)
+import Control.Category (id)
+import Control.Arrow ((>>>), (***), arr)
+import Data.Monoid (mempty, mconcat)
 
 import Hakyll
 
@@ -15,18 +20,44 @@ main = hakyll $ do
         route idRoute
         compile compressCssCompiler
 
-    -- Render pages
-    match "pages/index.markdown" $ do
-        route   $ constRoute "index.html"
+    -- Render posts
+    match "posts/*" $ do
+        route   $ setExtension ".html"
         compile $ pageCompiler
+            >>> myMetadataA
+            >>> arr (renderDateField "date" "%e %b %Y" "Unknown Date")
+            >>> applyTemplateCompiler "templates/post.html"
             >>> applyTemplateCompiler "templates/default.html"
             >>> relativizeUrlsCompiler
 
-    match "pages/about.markdown" $ do
-        route   $ constRoute "about.html"
-        compile $ pageCompiler
-            >>> applyTemplateCompiler "templates/default.html"
-            >>> relativizeUrlsCompiler
+    -- Render index
+    match "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> myMetadataA
+        >>> requireAllA "posts/*" addPostList
+        >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
 
     -- Compile templates
     match "templates/*" $ compile templateCompiler
+
+-- Site configuration metadata
+myMetadataA = arr (trySetField "brand" "mateusz.loskot.net")
+    >>> arr (trySetField "homeurl" "/")
+    >>> arr (trySetField "blogurl" "/blog.html")
+    >>> arr (trySetField "author" "Mateusz Loskot")
+    >>> arr (renderDateField "date" "%e %b %Y" "Unknown Date")
+
+-- | Helper functions
+--
+
+-- | Auxiliary compiler: generate a post list from a list of given posts, and
+-- add it to the current page under @$posts@
+--
+addPostList :: Compiler (Page String, [Page String]) (Page String)
+addPostList = setFieldA "posts" $
+    arr (reverse . chronological)
+        >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
+        >>> arr mconcat
+        >>> arr pageBody
---
+++ posts/2011-11-07-new-soci-project-leader-needed.markdown
@@ -0,0 +1,21 @@
+---
+comments: true
+date: 2011-11-07 01:01:53
+layout: post
+slug: new-soci-project-leader-needed
+title: New SOCI Project Leader needed
+wordpressid: 2260
+categories: programming
+tags: backend c++ database dbms project soci
+---
+
+[Maciej Sobczak](http://www.msobczak.com/), current leader of [SOCI](http://soci.sourceforge.net/) project has just [announced on soci-users mailing list](http://sourceforge.net/mailarchive/forum.php?thread_name=4EB709EF.7070602@msobczak.com&forum_name=soci-users) that he is looking _for a volunteer willing to take over the project leadership_. For someone, who would like to take over the project management responsibilities and has _a good understanding of the subject as well as a solid vision of what to do with the project in the future_.
+
+
+
+
+
+SOCI is a database access library for C++ that makes the illusion of embedding [SQL](http://en.wikipedia.org/wiki/SQL) queries in the regular C++ code, staying entirely within the [Standard C++](http://en.wikipedia.org/wiki/C++).
+
+
+
