399e4718d1675c736f838407bdb377b3cf987875
Mateusz Loskot <mateusz@loskot.net>
2017-06-24 00:21:04 +0200

    Fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 # deploy.sh - https://github.com/mloskot/mateusz.loskot.net
 set -e
 # deployment via FTPS
-lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror -eRv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN >/dev/null 2>&1
+lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror -eRv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
