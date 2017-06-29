e308e0b8f576ecf13ba2766f0417d756f10d1c76
Mateusz Loskot <mateusz@loskot.net>
2017-06-29 10:11:05 +0200

    [deploy] Investigating script failure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,6 @@
 # deploy.sh - https://github.com/mloskot/mateusz.loskot.net
 set -e
 # deployment via FTPS
-lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror -eRv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
+echo "Deploying public folder..."
+echo "PWD=$PWD"
+#lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror -eRv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
