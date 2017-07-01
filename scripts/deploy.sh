991fd11f5f2c32ff768774b116209b27c63e84a1
Mateusz Loskot <mateusz@loskot.net>
2017-07-01 22:07:57 +0200

    Fix script deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,4 @@
 set -e
 # deployment via FTPS
 echo "Deploying public folder..."
-echo "PWD=$PWD"
-#lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror -eRv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
+lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror -eRv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
