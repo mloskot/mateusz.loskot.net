70f5fd90d59407f091b326e9114b27ee07629919
Mateusz Łoskot <mateusz@loskot.net>
2019-05-24 19:31:14 +0200

    Run lftp with verbose=1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 set -e
 # deployment via FTPS
 echo "Deploying public folder..."
-lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror --exclude tmp/ --exclude-glob ".ftpquota" --exclude-glob ".htaccess" -Rv public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
+lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror --exclude tmp/ --exclude-glob ".ftpquota" --exclude-glob ".htaccess" -R --verbose=1 public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
