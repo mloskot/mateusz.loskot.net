029642e83942ced0dd6ef7e2382e47ed788e1d14
Mateusz Łoskot <mateusz@loskot.net>
2019-05-25 23:48:04 +0200

    Tidy up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # deploy.sh - https://github.com/mloskot/mateusz.loskot.net
 set -e
-# deployment via FTPS
 echo "Deploying public folder..."
-lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror --exclude tmp/ --exclude-glob ".ftpquota" --exclude-glob ".htaccess" -R --verbose=1 public .; quit;' -u $FTP_BREXITKNEESHOT:$FTP_FUCKYOUNESTLE $FTP_WHEREBLACKHOLESUN
+lftp -e 'set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate no; mirror --exclude tmp/ --exclude-glob ".ftpquota" --exclude-glob ".htaccess" -R --verbose=1 public .; quit;' -u $FUCKYOUNESTLE:$BREXITKNEESHOT $WHEREBLACKHOLESUN
